32b9387ef67c2f0693040edad7d9b5cf39c99dff
fvinodh1978 <f.vinodhfranklin@gmail.com>
2023-05-13 21:33:01 +0530

    Added tutorial files

--- Gift/src/KeywordLibrary/UIKeywords.py
+++ Gift/src/KeywordLibrary/UIKeywords.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 
-def get_browser():
-    service = Service('Gift/lib/chromedriver.exe')
-    browser = webdriver.Chrome(service=service)
-    return browser
+class ui_keywords:
+    def __init__(self):
+        pass
+
+    def get_browser(self):
+        service = Service('Gift/lib/chromedriver.exe')
+        browser = webdriver.Chrome(service=service)
+        return browser
+
+    def launch_ui(self, url, user, passwd):
+        browser = self.get_browser()
+        browser.get(url)
+        print(browser.title)
+        print(browser.title)
 
 
-def launch_ui(url, user, passwd):
-    browser = get_browser()
-    browser.get(url)
-    print(browser.title)
-    print(browser.title)
-
-
-url = 'https://www.google.com'
-launch_ui(url, 'a', 'b')
---
+++ Gift/src/ScriptExecutor.py
@@ -0,0 +1,5 @@
+import KeywordLibrary.UIKeywords
+
+uk = KeywordLibrary.UIKeywords.ui_keywords()
+url = 'https://www.google.com'
+uk.launch_ui(url, 'a', 'b')
