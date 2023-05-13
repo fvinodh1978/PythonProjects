2ebe6674ee4861eeeb56841beb544cc8d663896c
fvinodh1978 <f.vinodhfranklin@gmail.com>
2023-05-13 20:58:42 +0530

    Added get_browser function

--- Gift/src/KeywordRepository/UIKeywords.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-service = Service('Gift/lib/chromedriver.exe')
-browser = webdriver.Chrome(service=service)
-url = 'https://www.google.com'
-browser.get(url)
-print(browser.title)
-print(browser.title)
