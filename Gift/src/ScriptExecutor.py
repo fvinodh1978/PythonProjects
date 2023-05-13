ab548873358f94c101117b0d2a6ff4aa8ffd1748
fvinodh1978 <f.vinodhfranklin@gmail.com>
2023-05-13 22:47:37 +0530

    [ITRACK001] Changed the values of userid and password in the file ScriptExecutor.py

--- Gift/src/ScriptExecutor.py
+++ Gift/src/ScriptExecutor.py
@@ -2,5 +2,5 @@ import KeywordLibrary.UIKeywords
 
 uk = KeywordLibrary.UIKeywords.ui_keywords()
 url = 'https://www.google.com'
-title = uk.launch_ui(url, 'a', 'b')
+title = uk.launch_ui(url, 'abc', 'bcd')
 print(title)
